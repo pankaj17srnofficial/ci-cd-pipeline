0857166376e692883a2386b6b9409891ca99332a
pankaj sharma <pankaj.sharma@sroniyan.com>
2024-04-21 20:40:58 +0530

    launch ci-cd-automation

---
+++ cicd-automation-service/src/main/java/com/exe/cicd/cicdautomationservice/CicdAutomationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exe.cicd.cicdautomationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAutomationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAutomationServiceApplication.class, args);
+	}
+
+}
